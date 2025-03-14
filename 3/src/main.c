1dbc8f437699016825a88dfb855211bd8b3f9d74
parsee <babakovi679@gmail.com>
2025-03-14 21:38:51 +0300

    initial structutre

---
+++ 3/src/main.c
@@ -0,0 +1,17 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "logger/logger.h"
+
+int main(void)
+{
+    log_create();
+
+    
+
+    log_destruct();
+
+    return EXIT_SUCCESS;
+}
